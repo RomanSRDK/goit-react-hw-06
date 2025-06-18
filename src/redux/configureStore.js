1604bede5f80a88baf67b8160634f405d79a15c3
Roman Serdiuk <roman.srdk21@gmail.com>
2025-06-18 16:46:39 -0400

    Persist_DONE

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,25 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
 import filterReducer from "./filtersSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["items"],
+};
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
     filter: filterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// const persistConfig = {
-//   key: "contacts",
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
-// export default () => {
-//   let store = createStore(persistedReducer);
-//   let persistor = persistStore(store);
-//   return { store, persistor };
-// };
+export const persistor = persistStore(store);
